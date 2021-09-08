92098b59dd3332d0b0e41c3aa71253712a3c8db5
NakataKazuya <nakata_kazuya@lightcafe.co.jp>
2021-09-08 16:08:18 +0900

    [add]ログイン機能追加(Dao)
    DBから該当するエンティティを取得するメソッドを追記

--- src/main/java/com/example/demo/repository/UserDao.java
+++ src/main/java/com/example/demo/repository/UserDao.java
@@ -2,6 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.User;
 
+import java.util.Optional;
+
 public interface UserDao {
   int insert(User user);
+  Optional<User> findUser(User user);
 }
