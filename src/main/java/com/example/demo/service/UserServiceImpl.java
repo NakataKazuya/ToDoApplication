ab1fca805caeb7df6a0e5c8607f4c82fb5ad9dc3
NakataKazuya <nakata_kazuya@lightcafe.co.jp>
2021-09-08 16:09:56 +0900

    [add]ログイン機能(Service)
    ログイン機能のService部分の追記。(Daoから処理を呼び出す＋例外処理)

--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -3,7 +3,16 @@ package com.example.demo.service;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserDao;
 
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -18,5 +27,15 @@ public class UserServiceImpl implements UserService {
   public void insert(User user) {
     dao.insert(user);
   }
-  
+
+  @Override
+  public User loadUserByUsername(User user) throws EmptyResultDataAccessException {
+
+    try {
+      return dao.findUser(user).get();
+    } catch (EmptyResultDataAccessException e) {
+      throw new TaskNotFoundException("誤りです");
+    }
+  }
+
 }
